618ebb142e21acae78ced38a05c3dbe1aa996904
Youssef Alboghdady <youssefalboghdady399@gmail.com>
2025-02-17 13:59:15 +0200

    firstCommit

---
+++ src/main/java/com/boghdady/jwtdemo/services/UserService.java
@@ -0,0 +1,25 @@
+package com.boghdady.jwtdemo.services;
+
+import com.boghdady.jwtdemo.entities.User;
+import com.boghdady.jwtdemo.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
